4e274a6388fb462ea5bb49740249ffbd8cbf69ef
AbdallhOmran <171288488+AbdallhOmran@users.noreply.github.com>
2026-08-01 09:03:43 +0300

    feat: implement database connection utility using Mongoose and dotenv

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,34 +1,12 @@
-// const mongoose = require("mongoose");
-// require("dotenv").config();
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MYCONNECTION);
-//     console.log("Connected to database");
-//   } catch (err) {
-//     console.error(err);
-//   }
-// };
-
-// module.exports = connectDB;
-
-
 const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connectDB = async () => {
-  console.log("========== DB ==========");
-  console.log("MYCONNECTION exists:", !!process.env.MYCONNECTION);
-  console.log("MYCONNECTION:", process.env.MYCONNECTION?.slice(0, 40));
-
   try {
     await mongoose.connect(process.env.MYCONNECTION);
-
-    console.log("✅ Connected to MongoDB");
+    console.log("Connected to database");
   } catch (err) {
-    console.error("❌ Mongo Error:");
     console.error(err);
-    throw err;
   }
 };
 
