93e36e1f0e32f790f32461dfe2888942fb5b1f44
AbdallhOmran <171288488+AbdallhOmran@users.noreply.github.com>
2026-08-01 08:41:42 +0300

    feat: initialize environment configuration with production API endpoint

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
+  // apiUrl: 'http://localhost:3000',
+ apiUrl: 'https://asset-link-deploy.vercel.app',
   // apiUrl: 'https://asset-link-api.vercel.app',
 };
